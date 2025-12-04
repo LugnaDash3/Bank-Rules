a2eaaa90a00b047b94a13ce25861904859b36f68
LugnaDash3 <lugnadash3@gmail.com>
2025-12-04 23:40:16 +0530

    Create PayTM.java

---
+++ PayTM.java
@@ -0,0 +1,9 @@
+
+class PayTM extends SBI 
+{
+    void addRule() 
+    {
+        System.out.println("PayTM Additional Rule Applied: Full KYC required for wallet to bank transfers.");
+        System.out.println("PayTM Additional Rule Applied: Maximum wallet balance is ₹1,00,000 after full KYC.");
+    }
+}
