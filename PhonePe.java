1ab3a9203bd789f92b6386fa77f3504cefbe347f
LugnaDash3 <lugnadash3@gmail.com>
2025-12-04 23:37:34 +0530

    Create PhonePe.java

---
+++ PhonePe.java
@@ -0,0 +1,9 @@
+
+class PhonePe extends SBI
+{
+    void addRule() 
+    {
+        System.out.println("PhonePe Additional Rule Applied: Cashback valid only on selected merchants.");
+        System.out.println("PhonePe Additional Rule Applied: Wallet balance limit is ₹10,000.");
+    }
+}
