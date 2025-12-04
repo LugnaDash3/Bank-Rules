6dccab56d643eebd53c08d8090c7d572e84ff774
LugnaDash3 <lugnadash3@gmail.com>
2025-12-04 23:38:48 +0530

    Create GPay.java

---
+++ GPay.java
@@ -0,0 +1,9 @@
+
+class GPay extends SBI 
+{
+    void addRule() 
+    {
+        System.out.println("GPay Additional Rule Applied: Users can send money only to UPI-verified accounts.");
+        System.out.println("GPay Additional Rule Applied: Scratch card rewards depend on transaction amount.");
+    }
+}
