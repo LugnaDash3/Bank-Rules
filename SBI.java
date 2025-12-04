22a00888f70e7e8984de82a2966c4774c37b993e
LugnaDash3 <lugnadash3@gmail.com>
2025-12-04 23:36:03 +0530

    Create SBI.java

---
+++ SBI.java
@@ -0,0 +1,13 @@
+class SBI 
+{
+    void actRule() 
+   {
+        System.out.println("SBI Account Rule Applied: Minimum balance must be maintained as per SBI policy.");
+        System.out.println("SBI Account Rule Applied: KYC verification is mandatory for all accounts.");
+    }
+    void transRule() 
+   {
+        System.out.println("SBI Transaction Rule Applied: Daily UPI transaction limit up to ₹1,00,000.");
+        System.out.println("SBI Transaction Rule Applied: Transactions above ₹50,000 require PAN details.");
+    }
+}
